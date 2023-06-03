a313d07505e8d8d5d70f73c7f41922c0dc85a841
Engi Lozada <engilozada@gmail.com>
2023-06-03 22:56:07 +0200

    Styling Write page

--- client/src/pages/Write.js
+++ client/src/pages/Write.js
@@ -1,7 +1,55 @@
-import React from "react";
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import "./styles/Write.css";
 
 const WritePage = () => {
-  return <div>Write</div>;
+  const [value, setValue] = useState("");
+  return (
+    <div className="add">
+      <div className="content">
+        <input type="text" placeholder="Title" />
+        <div className="editorContainer">
+          <ReactQuill
+            className="editor"
+            theme="snow"
+            value={value}
+            onChange={setValue}
+          />
+        </div>
+      </div>
+      <div className="menu">
+        <div className="item">
+          <h1>Publish</h1>
+          <span>
+            <b>Status:</b> Draft
+          </span>
+          <span>
+            <b>Visibility:</b> Public
+          </span>
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label className="file" htmlFor="file">
+            Upload Image
+          </label>
+          <div className="buttons">
+            <button className="save">Save as a draft</button>
+            <button className="update">Update</button>
+          </div>
+        </div>
+        <div className="item">
+          <h1>Category</h1>
+          <div className="cat">
+            <input type="radio" name="cat" value="recipe" id="recipe" />
+            <label htmlFor="recipe">Recipe</label>
+          </div>
+          <div className="cat">
+            <input type="radio" name="cat" value="blog" id="blog" />
+            <label htmlFor="blog">Blog</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WritePage;
