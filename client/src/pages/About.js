958c9eeaf14e7042d13bc8ccbaa1a077bcfddb9e
Engi Lozada <engilozada@gmail.com>
2023-06-03 23:50:17 +0200

    Styling about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,5 +1,87 @@
+import aboutImg1 from "../assets/about1.avif";
+import aboutImg2 from "../assets/about2.avif";
+import aboutImg3 from "../assets/about3.avif";
+import "./styles/About.css";
+
 const AboutPage = () => {
-  return <div>About</div>;
+  return (
+    <div className="home">
+      <div className="posts">
+        <div className="post">
+          <div className="img">
+            <img src={aboutImg1} alt="img" />
+          </div>
+          <div>
+            <h1>Hello</h1>
+            <p>
+              Welcome to Valentina Nutrition! My name is Valentina, and I'm a
+              registered dietitian and nutritionist with a passion for helping
+              individuals achieve their health and wellness goals through
+              personalized nutrition plans and evidence-based advice. I believe
+              that proper nutrition is the foundation for a healthy lifestyle,
+              and my mission is to empower my clients with the knowledge and
+              tools they need to make informed decisions about their diet and
+              nutrition. Whether you're looking to lose weight, manage a chronic
+              condition, or simply adopt a healthier way of eating, I'm here to
+              guide you on your journey. My approach to nutrition is holistic
+              and individualized, taking into account each client's unique
+              needs, preferences, and lifestyle. I believe in the power of
+              whole, nutrient-dense foods, and I strive to make healthy eating
+              both delicious and achievable for everyone.
+            </p>
+          </div>
+        </div>
+        <div className="post">
+          <div className="img">
+            <img src={aboutImg2} alt="img" />
+          </div>
+          <div>
+            <h1>Hello</h1>
+            <p>
+              Welcome to Valentina Nutrition! My name is Valentina, and I'm a
+              registered dietitian and nutritionist with a passion for helping
+              individuals achieve their health and wellness goals through
+              personalized nutrition plans and evidence-based advice. I believe
+              that proper nutrition is the foundation for a healthy lifestyle,
+              and my mission is to empower my clients with the knowledge and
+              tools they need to make informed decisions about their diet and
+              nutrition. Whether you're looking to lose weight, manage a chronic
+              condition, or simply adopt a healthier way of eating, I'm here to
+              guide you on your journey. My approach to nutrition is holistic
+              and individualized, taking into account each client's unique
+              needs, preferences, and lifestyle. I believe in the power of
+              whole, nutrient-dense foods, and I strive to make healthy eating
+              both delicious and achievable for everyone.
+            </p>
+          </div>
+        </div>
+        <div className="post">
+          <div className="img">
+            <img src={aboutImg3} alt="img" />
+          </div>
+          <div>
+            <h1>Hello</h1>
+            <p>
+              Welcome to Valentina Nutrition! My name is Valentina, and I'm a
+              registered dietitian and nutritionist with a passion for helping
+              individuals achieve their health and wellness goals through
+              personalized nutrition plans and evidence-based advice. I believe
+              that proper nutrition is the foundation for a healthy lifestyle,
+              and my mission is to empower my clients with the knowledge and
+              tools they need to make informed decisions about their diet and
+              nutrition. Whether you're looking to lose weight, manage a chronic
+              condition, or simply adopt a healthier way of eating, I'm here to
+              guide you on your journey. My approach to nutrition is holistic
+              and individualized, taking into account each client's unique
+              needs, preferences, and lifestyle. I believe in the power of
+              whole, nutrient-dense foods, and I strive to make healthy eating
+              both delicious and achievable for everyone.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import "./styles/auth.css";
+import "./styles/Auth.css";
 
 const LoginPage = () => {
   return (
